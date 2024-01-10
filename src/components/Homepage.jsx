4aa2c850b070358818cf292d29334ed4012d069e
nicola <you@example.com>
2024-01-10 17:43:05 +0000

    cleaned up code

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,71 +1,58 @@
+import React, { useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
-import React, { useState, useEffect } from 'react';
-import { Form,Button,Container,Row,Col} from 'react-bootstrap';
+import { fetchRecipebyFood } from "../utils/api";
+import RecipeCard from "./RecipeCards";
 
-import { fetchRecipebyFood } from '../utils/api';
-import RecipeCard from './RecipeCards';
+const Homepage = () => {
+  const [foodChoice, setFoodChoice] = useState("");
+  const [recipeArray, setRecipeArray] = useState([]);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchRecipebyFood(foodChoice).then((newRecipe) => {
+      setRecipeArray(newRecipe);
+      console.log(recipeArray);
+    });
+  };
 
-
-const Homepage =() =>{
-
-
-    const[foodChoice, setFoodChoice] = useState("")
-    const[recipeArray, setRecipeArray]= useState([])
-
-    const handleSubmit =(event) =>{
-        event.preventDefault();
-        fetchRecipebyFood(foodChoice).then((newRecipe)=>{
-          setRecipeArray(newRecipe)
-      console.log(recipeArray)
-            
-        })
-       
-
-    }
-    // useEffect(() => {
-    //     console.log(recipeArray);
-    //   }, [recipeArray]);
-    return (
-    
-     <>
-        <p align="center" className="p-3"> Welcome to the Recipe Finder</p>
-        <Container align="center">
+  return (
+    <>
+      <p align="center" className="p-3">
+        {" "}
+        Welcome to the Recipe Finder
+      </p>
+      <Container align="center">
         <Form className="p-3" onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasic">
-          <Form.Label>Enter a food to search for:</Form.Label>
-          <Form.Control
-          
-            type="foodChoice"
-            placeholder="Enter a food here"
-            value={foodChoice}
-            onChange={(e) => setFoodChoice(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Button variant="success" className="m-3" onClick={handleSubmit}>
-          Submit
-        </Button>
+          <Form.Group controlId="formBasic">
+            <Form.Label>Enter a food to search for:</Form.Label>
+            <Form.Control
+              type="foodChoice"
+              placeholder="Enter a food here"
+              value={foodChoice}
+              onChange={(e) => setFoodChoice(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Button variant="success" className="m-3" onClick={handleSubmit}>
+            Submit
+          </Button>
         </Form>
-        </Container>
-  
-        <Row xs={1} md={3} className="g-4">
-            
-        {recipeArray.map((item) => (  
-              <Col key={item.recipe.label}>
-                 <RecipeCard item={item} recipeArray={recipeArray} setRecipeArray={setRecipeArray}/>
-                 </Col>
-        ) )}
+      </Container>
 
-        </Row>
-        </>
-        
-     
-     
-        
-    )
-
+      <Row xs={1} md={3} className="g-4">
+        {recipeArray.map((item) => (
+          <Col key={item.recipe.label}>
+            <RecipeCard
+              item={item}
+              recipeArray={recipeArray}
+              setRecipeArray={setRecipeArray}
+            />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
-
 
 export default Homepage;
--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,44 +1,35 @@
-import {Button, Container, Row, Col, Card} from 'react-bootstrap'
-import { useContext, useEffect, useState } from "react"
-
-import { RecipeContext } from '../MealContext';
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
 
 
-function Planner ({ currentItem }){
 
+
+function Planner({ currentItem }) {
  
 
-      
-
-
- 
-return (
+  return (
     <>
-<Container >
-
-     
-    
-    
-    <Row >
-        <Col>
-            <Card> 
-             <Card.Body>
-                 <h3>  {currentItem.day}</h3> 
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <h3> {currentItem.day}</h3>
                 <p> {currentItem.name} </p>
-                <Button href={currentItem.url} target="_blank" rel="noopener noreferrer" > Recipe Link</Button>
-             
-                   
-               </Card.Body>
-                </Card>
-               </Col>
-                </Row>
-           
-          
-        </Container>
-       
-
-</>
-    )
-    };
+                <Button
+                  href={currentItem.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  Recipe Link
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
 
 export default Planner;
